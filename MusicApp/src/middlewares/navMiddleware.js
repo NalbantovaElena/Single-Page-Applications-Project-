778cfad92a03bf0d4406f047bf41a701e147c21a
NalbantovaElena <48248363+NalbantovaElena@users.noreply.github.com>
2022-04-13 10:36:50 +0300

    Create navMiddleware.js

---
+++ MusicApp/src/middlewares/navMiddleware.js
@@ -0,0 +1,8 @@
+import { render } from '../../node_modules/lit-html/lit-html.js';
+import { navigationView } from '../views/navigationView.js';
+const headerElement = document.querySelector('.header-navigation')
+export const renderNavigation = (ctx, next) => {
+    //TODO: render navigation
+    render(navigationView(ctx), headerElement)
+    next()
+};
